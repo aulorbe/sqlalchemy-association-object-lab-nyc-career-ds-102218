43928321cf5728d3110b0288390c3a50674ea80e
aulorbe <aulorbe@gmail.com>
2018-11-06 17:06:14 -0500

    finished schema

--- models.py
+++ models.py
@@ -4,11 +4,32 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
-# Write your classes below
+# An Artist has many Songs and has many Genres through Songs
+# A Genre has many Songs and has many Artists through Songs
+# Every Song belongs to an Artist and belongs to a Genre
 
+class Artist(Base):
+    __tablename__ = 'artists'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    songs = relationship('Song', back_populates='artist') # back populates to the artist property in the song model
+    genres = relationship('Genre', secondary='songs', back_populates='artists') # uses the songs join table in order to create a has-many relationship with the Genres class and the artists variable in the Genre class
 
+class Song(Base): # this class will be 'mapped' and used as our join table
+    __tablename__ = 'songs'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    artist = relationship('Artist', back_populates = 'songs') # this is the artist property in the song model
+    genre = relationship('Genre', back_populates = 'songs')
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+    genre_id = Column(Integer, ForeignKey('genres.id'))
 
-
+class Genre(Base):
+    __tablename__ = 'genres'
+    id = Column(Integer, primary_key=True)
+    name = Column(Text)
+    songs = relationship('Song', back_populates='genre') #back populates to the genre property in the Song class (because genre has many songs though the Song class)
+    artists = relationship('Artist', secondary='songs', back_populates='genres') # back populates to the genres variable in the Artist class
 
 
 engine = create_engine('sqlite:///:memory:')
